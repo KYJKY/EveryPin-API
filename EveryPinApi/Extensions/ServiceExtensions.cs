cbfa87cb6e33947ff911934e23ec9f0aaccd89cc
PC_KYJ <illulizer@gmail.com>
2023-12-02 22:22:19 +0900

    - Feat: Swagger, CORS 설정 추가 - Test: Azure 로깅 테스트 설정 추가

---
+++ EveryPinApi/Extensions/ServiceExtensions.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Logging.AzureAppServices;
+
+namespace EveryPinApi.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureCors(this IServiceCollection services) =>
+        services.AddCors(options =>
+        {
+            // Cors 관련 설정
+
+            // 현재는 모든 도메인을 CORS 정책으로부터 허용함
+            options.AddPolicy("CorsPolicy", builder =>
+            builder.AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader());
+
+            // 특정 도메인만 허용하도록 수정
+            //options.AddPolicy("CorsPolicy", builder =>
+            //builder.WithOrigins("https://example.com")      // 입력한 도메인만 접근 가능
+            //.WithMethods("POST", "GET")                     // POST, GET 방식만 접근 가능
+            //.WithHeaders("accept", "content-type"));        // 특정 헤더만 허용
+
+        });
+
+        public static void ConfigureLogger(this IServiceCollection services) =>
+        services.Configure<AzureFileLoggerOptions>(options =>
+        {
+            // Azure Logging 관련 설정
+            options.FileName = "logs-";
+            options.FileSizeLimit = 50 * 1024;      // 파일 크기 제한
+            options.RetainedFileCountLimit = 5;     // 최대 보존 파일 수
+        });
+    }
+}
--- EveryPinApi/Program.cs
+++ EveryPinApi/Program.cs
@@ -1,15 +1,44 @@
+using EveryPinApi.Extensions;
+using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.Extensions.Logging.ApplicationInsights;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.ConfigureCors();       // CORS
+
+
 builder.Services.AddControllers();
+// Swagger/OpenAPI 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+// Azure Logging
+builder.Services.ConfigureLogger();
+builder.Logging.AddAzureWebAppDiagnostics();
+
+
+
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseStaticFiles();
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.All
+});
+
+app.UseCors("CorsPolicy");
 
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();

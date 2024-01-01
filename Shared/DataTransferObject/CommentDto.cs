5de6a26ba0a71399e671544754a1e50742594555
KYJ_Laptop <illulizer@gmail.com>
2024-01-01 16:13:20 +0900

    Feat: Dto 폴더 생성

---
+++ Shared/DataTransferObject/CommentDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.DataTransferObject
+{
+    public record CommentDto(Guid Id, Guid UserId, string? CommentMessage, DateTime? CreatedDate)
+    {
+        //public Guid Id { get; set; }
+        //public Guid UserId { get; set; }
+        //public string? CommentMessage { get; set; }
+        //public DateTime? CreatedDate { get; set; }
+    }
+}

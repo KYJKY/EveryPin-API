5735594c2d2cec61885abdaf3bb31594e4c5fccc
PC_KYJ <illulizer@gmail.com>
2024-07-16 03:47:52 +0900

    Feat: Blob Storage 서비스 추가

---
+++ Shared/DataTransferObject/Blob/BlobResponseDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.DataTransferObject.Blob
+{
+    public class BlobResponseDto
+    {
+        public BlobDto Blob { get; set; }
+        public string? Status { get; set; }
+        public bool Error {  get; set; }
+
+        public BlobResponseDto()
+        {
+            Blob = new BlobDto();
+        }
+    }
+}
